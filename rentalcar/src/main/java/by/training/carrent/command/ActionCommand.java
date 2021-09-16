52a229287d2c3b4587cf588808df61a226cbd5d6
Evgeniy <koroza1995@gmail.com>
2021-09-16 08:19:21 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/command/ActionCommand.java
+++
@@ -1,9 +0,0 @@
-package by.training.carrent.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-import by.training.carrent.controller.Router;
-
-public interface ActionCommand {
-	Router execute(HttpServletRequest request);
-}
--- rentalcar/src/main/java/by/training/carrent/command/PageUrl.java
+++
@@ -1,7 +0,0 @@
-package by.training.carrent.command;
-
-public final class PageUrl {
-	private static final String SIGN_UP = "/pages/sign_up.jsp";
-	private PageUrl() {
-	}
-}
