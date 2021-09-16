ec938326f50463d89724e4b4478152037583be58
Evgeniy <koroza1995@gmail.com>
2021-09-16 08:18:08 +0300

    *

---
+++ rentalcar/src/main/java/by/training/carrent/controller/listener/ServletContextListenerImpl.java
@@ -0,0 +1,19 @@
+package by.training.carrent.controller.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import by.training.carrent.model.connection.ConnectionPool;
+
+@WebListener
+public class ServletContextListenerImpl implements ServletContextListener {
+	@Override
+    public void contextInitialized(ServletContextEvent sce)  { 
+        ConnectionPool.getInstance();
+    }
+	@Override
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	ConnectionPool.getInstance().destroyPool();
+    }
+}
