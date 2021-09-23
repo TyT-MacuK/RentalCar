e862bccd5c2ef8927356ff973657cf3d8b020f9d
Evgeniy <koroza1995@gmail.com>
2021-09-23 08:27:54 +0300

    *

---
+++ rentalcar/src/main/java/by/training/carrent/controller/filter/EncodingFilter.java
@@ -0,0 +1,41 @@
+package by.training.carrent.controller.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+@WebFilter(urlPatterns = { "/*" }, initParams = {
+		@WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding Param") })
+public class EncodingFilter implements Filter {
+	private static final String ENCODING_PARAMETER = "encoding";
+    private static final String ENCODING = "UTF-8";
+	private String code;
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		code = fConfig.getInitParameter("ENCODING_PARAMETER");
+		if (code == null) {
+			code = ENCODING;
+		}
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String codeRequest = request.getCharacterEncoding();
+		if (!code.equalsIgnoreCase(codeRequest)) {
+			request.setCharacterEncoding(code);
+			response.setCharacterEncoding(code);
+		}
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+		code = null;
+	}
+
+}
