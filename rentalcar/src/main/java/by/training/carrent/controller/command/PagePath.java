bc982186207b8cac3574ec0f42fcf8af0906cdfc
Evgeniy <koroza1995@gmail.com>
2021-09-23 08:27:35 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/controller/command/PagePath.java
+++ rentalcar/src/main/java/by/training/carrent/controller/command/PagePath.java
@@ -9,6 +9,15 @@ public final class PagePath {
 	public static final String HOME_PAGE = "/pages/home.jsp";
 	public static final String HOME_PAGE_REDIRECT = "/rentalcar/controller?command=to_home_page_command";
 	
+	public static final String SIGN_IN_PAGE = "/pages/sign_in.jsp";
+	public static final String SIGN_IN_PAGE_REDIRECT = "/rentalcar/controller?command=to_sign_in_page_command";
+	
+	public static final String PERSONAL_PROFILE_REDIRECT = "/rentalcar/controller?command=to_personal_profile_page_command";
+	public static final String PERSONAL_PROFILE_PAGE = "/pages/personal_profile.jsp";
+	
+	public static final String CHANGE_FIRST_NAME_REDIRECT = "/rentalcar/controller?command=to_change_first_name_page_command";
+	public static final String CHANGE_FIRST_NAME_PAGE = "/pages/change_first_name.jsp";
+	
 	public static final String ERROR_500_PAGE = "pages/error/error500.jsp";
 	
 	public static final String ERROR_403_PAGE = "pages/error/error403.jsp";
--- rentalcar/src/main/java/by/training/carrent/controller/command/RequestParameter.java
+++ rentalcar/src/main/java/by/training/carrent/controller/command/RequestParameter.java
@@ -1,22 +1,24 @@
 package by.training.carrent.controller.command;
 
 public final class RequestParameter {
-	 public static final String USER_ID = "id";
-	    public static final String USER_EMAIL = "email";
-	    public static final String USER_PASSWORD = "password";
-	    public static final String USER_FIRST_NAME = "first_name";
-	    public static final String USER_LAST_NAME = "last_name";
-	    public static final String USER_DATE_OF_BIRTH = "date_of_birth";
-	    public static final String USER_PHONE_NUMBER = "phone_number";
-	    public static final String USER_DISCOUNT = "discount";
-	    public static final String USER_ROLE = "role";
-	    public static final String USER_STATUS = "status";
-	    
-	    public static final String CODE = "code";
-	    public static final String COMMAND = "command";	
-	    
+	public static final String USER_ID = "id";
+	public static final String USER_EMAIL = "email";
+	public static final String USER_PASSWORD = "password";
+	public static final String USER_FIRST_NAME = "first_name";
+	public static final String USER_LAST_NAME = "last_name";
+	public static final String USER_DATE_OF_BIRTH = "date_of_birth";
+	public static final String USER_PHONE_NUMBER = "phone_number";
+	public static final String USER_DISCOUNT = "discount";
+	public static final String USER_ROLE = "role";
+	public static final String USER_STATUS = "status";
+
+	public static final String CODE = "code";
+	public static final String COMMAND = "command";
 	
-	private RequestParameter () {
+	public static final String AUTHENTICATION_ERROR = "authentication_error";
+	public static final String CHANGE_ERROR = "change_error";
+
+	private RequestParameter() {
 	}
 
 }
