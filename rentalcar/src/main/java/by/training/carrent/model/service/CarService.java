ca6d3b9f50aad86803269e5fc1a72b3da475fe7f
Evgeniy <koroza1995@gmail.com>
2021-10-14 08:29:08 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/model/service/CarService.java
+++ rentalcar/src/main/java/by/training/carrent/model/service/CarService.java
@@ -11,25 +11,14 @@ import by.training.carrent.exception.ServiceException;
 import by.training.carrent.model.entity.Car;
 
 public interface CarService {
-	//List<Car> findAll() throws ServiceException;
 	
 	boolean add(Map<String, String> parametrs, Part part) throws ServiceException;
 	
-	List<Car> findByLimit(int leftBorder, int numberOfLines) throws ServiceException;
-	
 	Optional<Car> findById(long carId) throws ServiceException;
 	
-	List<Car> findByModel(String model) throws ServiceException;
-
-	List<Car> findByDiscount(int discount) throws ServiceException;
-
-	List<Car> findByYear(int year) throws ServiceException;
-
-	List<Car> findByCost(BigDecimal cost) throws ServiceException;
+	List<Car> findByLimit(int leftBorder, int numberOfLines) throws ServiceException;
 
 	List<Car> findByManufacture(String carManufacturer) throws ServiceException;
-
-	List<Car> findByStatus(String carStatus) throws ServiceException;
 	
 	Map<Long, Car> findCarsIdByUserId(List<Long> listCarsId) throws ServiceException;
 
