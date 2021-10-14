e35cb8e45413d5f76c9aed86dcd257e52d4908a8
Evgeniy <koroza1995@gmail.com>
2021-10-14 16:17:01 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/model/dao/UserDao.java
+++ rentalcar/src/main/java/by/training/carrent/model/dao/UserDao.java
@@ -1,6 +1,5 @@
 package by.training.carrent.model.dao;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +10,7 @@ public interface UserDao extends BaseDao<Long, User> {
 	boolean add(User user, String password, String passwordForAuthentication) throws DaoException;
 	
 	Optional<User> findByPasswordForAuthentication(String passwordForAuthentication) throws DaoException;
-	
-	List<User> findByDateOfBirth(LocalDate dateOfBirth) throws DaoException;
-	
+		
 	Optional<User> findByEmail(String email) throws DaoException;
 	
 	Optional<User> findByEmailAndPassword(String email, String password) throws DaoException;
