4b8e2fe6aae6ad91657f21b91731d806e77b0d13
Evgeniy <koroza1995@gmail.com>
2021-10-14 08:49:55 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/model/dao/BaseDao.java
+++ rentalcar/src/main/java/by/training/carrent/model/dao/BaseDao.java
@@ -1,13 +1,6 @@
 package by.training.carrent.model.dao;
 
-import java.util.List;
-import java.util.Optional;
-
-import by.training.carrent.exception.DaoException;
 import by.training.carrent.model.entity.AbstractEntity;
 
 public interface BaseDao<K, T extends AbstractEntity> {
-	//List<T> findAll()  throws DaoException;
-	Optional<T> findById(K id)  throws DaoException;
-	boolean remove(T t)  throws DaoException;
 }
