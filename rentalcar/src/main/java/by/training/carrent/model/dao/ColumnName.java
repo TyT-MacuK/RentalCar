6f319d2f7dedf061d772d1b80a41664afa9b262e
Evgeniy <koroza1995@gmail.com>
2021-10-25 13:37:55 +0300

    *

--- rentalcar/src/main/java/by/training/carrent/model/dao/ColumnName.java
+++ rentalcar/src/main/java/by/training/carrent/model/dao/ColumnName.java
@@ -1,7 +1,7 @@
 package by.training.carrent.model.dao;
 
 public final class ColumnName {
-	// Table: "users"
+	// Table: users
 	public static final String USER_ID = "user_id";
 	public static final String USER_EMAIL = "email";
 	public static final String USER_PASSWORD = "password";
@@ -14,13 +14,16 @@ public final class ColumnName {
 	public static final String USER_STATUS = "user_ststus_id";
 	public static final String USER_ROLE = "user_role_id";
 	public static final String COUNT_USERS = "count_users";
-	// Table: "user_status"
+
+	// Table: user_status
 	public static final String USER_STATUS_ID = "user_status_id";
 	public static final String USER_STATUS_NAME = "user_status";
-	// Table: "user_role"
+
+	// Table: user_role
 	public static final String USER_ROLE_ID = "role_id";
 	public static final String USER_ROLE_NAME = "role";
-	// Table: "cars"
+
+	// Table: cars
 	public static final String CAR_ID = "car_id";
 	public static final String CAR_MODEL = "model";
 	public static final String CAR_DISCOUNT = "discount";
@@ -32,13 +35,16 @@ public final class ColumnName {
 	public static final String CAR_MANUFACTURER = "car_manufacturer_id";
 	public static final String CAR_STATUS = "car_status_id";
 	public static final String COUNT_CARS = "count_cars";
-	// Table: "car_manufacturer"
+
+	// Table: car_manufacturer
 	public static final String CAR_MANUFACTURER_ID = "manufacturer_id";
 	public static final String CAR_MANUFACTURER_NAME = "manufacturer";
-	// Table: :car_status"
+
+	// Table: car_status
 	public static final String CAR_STATUS_ID = "car_status_id";
 	public static final String CAR_STATUS_NAME = "car_status";
-	// Table: "orders"
+
+	// Table: orders
 	public static final String ORDER_ID = "order_id";
 	public static final String ORDER_PRICE = "price";
 	public static final String ORDER_PICK_UP_DATE = "pick_up_date";
@@ -47,12 +53,14 @@ public final class ColumnName {
 	public static final String ORDER_CAR = "car_id";
 	public static final String ORDER_USER = "user_id";
 	public static final String COUNT_ORDERS = "count_orders";
-	// Table: "order_status"
+
+	// Table: order_status
 	public static final String ORDER_STATUS_ID = "order_status_id";
 	public static final String ORDER_STATUS_NAME = "order_status";
-    // Table: "payment_card"
+
+	// Table: payment_card
 	public static final String PAYMENT_CARD_BALANCE = "balance";
-	
+
 	private ColumnName() {
 	}
 }
